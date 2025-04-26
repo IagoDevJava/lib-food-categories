21e78659924578c6e8be9aba7a4ed60f284d0bdc
Иван Егоров <iago.devs@yandex.ru>
2025-04-26 12:47:58 +0300

    first commit

---
+++ src/main/java/com/egorov/lib_food_categories/LibFoodCategoriesApplication.java
@@ -0,0 +1,13 @@
+package com.egorov.lib_food_categories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibFoodCategoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibFoodCategoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egorov/lib_food_categories/LibFoodCategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egorov.lib_food_categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibFoodCategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
